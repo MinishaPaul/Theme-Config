170f9763bd5dbcfc357ad0dcd64d3b9e378ce9c2
Minisha P M <minisha@finsurge.tech>
2025-03-06 17:26:41 +0530

    release 3

--- dynamic-theme/projects/dynamic-theme-lib/src/public-api.ts
+++ dynamic-theme/projects/dynamic-theme-lib/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/dynamic-theme-lib.service';
 export * from './lib/dynamic-theme-lib.component';
 export * from './lib/dynamic-theme-lib.module';
+export * from './lib/directives/background-colour.directive';
+export * from './lib/directives/text-colour.directive';
+export * from './lib/services/colour.service';
